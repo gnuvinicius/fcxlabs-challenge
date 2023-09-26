bf0ae623dfb5c98bf243cab0d9c8f5584843ba11
Vinicius Claudino <gnu.vinicius@gmail.com>
2023-09-25 21:19:04 -0300

    wip

--- FCxLabs-Challenge/FCXL-Api/Applications/Controllers/AuthController.cs
+++ FCxLabs-Challenge/FCXL-Api/Applications/Controllers/AuthController.cs
@@ -53,31 +53,4 @@ public class AuthController : ControllerBase
             return BadRequest(new { code = 400, ex.Message });
         }
     }
-
-    [HttpPost("temp")]
-    [AllowAnonymous]
-    public IActionResult ConvertJsonToSQL(List<User4devModel> user4dev)
-    {
-        List<string> test = new(); 
-
-    user4dev.ForEach(u =>
-        {
-            test.Add(@$"INSERT INTO public.tb_user(
-                username, password, name, email, phone, cpf, mother_name, status, updated_at, created_at, birtday)
-                VALUES('{u.email}', '$2a$12$nD3QjUD1hWyevaGGhzPWy.umXeHuaP8LJ0r34Yp5Zt0jC42ITpAoy', '{u.nome}', '{u.email}', '{u.celular}', '{u.cpf}', '{u.mae}', 1, NULL, current_timestamp, '1977-03-07 00:00:00.00');");
-        });
-
-        return Ok(test);
-    }
-}
-
-
-public class User4devModel
-{
-    public string nome { get; set; }
-    public string cpf { get; set; }
-    public string data_nasc { get; set; }
-    public string mae { get; set; }
-    public string email { get; set; }
-    public string celular { get; set; }
 }
